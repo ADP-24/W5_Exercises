bc3b9a204cce1fb0b60013dfe45e49804ff853e2
ADP-24 <aperez@appdev.yearup.org>
2024-10-21 14:59:36 -0500

    Getting started with python!

---
+++ variable_examples.py
@@ -0,0 +1,39 @@
+# Variables for customer details
+customer_id = 98765
+
+first_name = "John"
+middle_name = "Michael"
+last_name = "Doe"
+
+gender = "Male" # Male, Female, Pefer not to answer
+
+date_of_birth = "1985-07-23"  # YYYY-MM-DD format
+
+drivers_license_number = "D12345678"
+
+auto_policy_number = "AUTO-98765432"
+
+# Variables for myself
+customer_id = 123456
+
+first_name = "Anthony"
+middle_name = "Daniel"
+last_name = "Perez"
+
+gender = "Male"
+
+date_of_birth = "1995-01-04"  # YYYY-MM-DD format
+
+drivers_license_number = "D98765432"
+
+auto_policy_number = "AUTO-12345678"
+
+# False, None, True, and, as, assert, async, await, break, class, continue, def, del
+#elif, else, except, finally, for, from, global, if, import, in, is, lambda, nonlocal, not, or, pass
+#raise, return, try, while, with, yield
+
+# def - Used to define a function. It is followed by the function name and a set of parentheses that may include parameters.
+# class - Used to define a new class, which is a blueprint for creating objects (instances).
+# if - Used to create a conditional statement that executes a block of code only if a specified condition evaluates to true.
+# for - Used to create a loop that iterates over a sequence (like a list, tuple, or string) or other iterable objects.
+# try - Used to start a block of code that will be tested for errors during execution. It can be followed by except, finally, or else to handle exceptions.^^
