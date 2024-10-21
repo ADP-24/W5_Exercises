1e9140052058d336905e668c9af954632c5cf62d
ADP-24 <aperez@appdev.yearup.org>
2024-10-21 14:20:20 -0500

    First python script :)

---
+++ hello.py
@@ -0,0 +1,16 @@
+print('Hello World!')
+
+message = 'Hello World!'
+print(message)
+
+dollars = 3
+cents = .50
+
+print(dollars + cents)
+
+dollars = '3'
+
+print(dollars + "dollars")
+print(message)
+
+print('3 dollars' + ' ' + 'fifty cents')
