f172a715e61935fc165e964224f530d222fc1816
ADP-24 <aperez@appdev.yearup.org>
2024-10-23 18:42:10 -0500

    Exercise 3A

---
+++ NumericConversions/conversion_tests.py
@@ -0,0 +1,18 @@
+# Description: This script tests various numeric  
+#              conversion techniques 
+# Author: Sam Q. Newprogrammer 
+
+a = "  101.1 " 
+b = '55' 
+c = "402 Stevens" 
+d = 'Number 5  '
+
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+
+print(a, float(a))
+print(b, int(b))
+print(c, int(c))
+print(d, int(d))
